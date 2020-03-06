a762e7e950a45e04fe53a5919fe69cca9a331ea7
Mohamed ASSAKOUR <G522782@mph.morpho.com>
2020-03-06 11:09:00 +0100

    adding tests and some refactoring

--- src/main/java/note/book/server/controller/dto/InterpreterRequestDto.java
+++ src/main/java/note/book/server/controller/dto/InterpreterRequestDto.java
@@ -1,14 +1,15 @@
 package note.book.server.controller.dto;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class InterpreterRequestDto {
 
     @NotEmpty
-    //@Pattern(regexp = "%[a-zA-Z]+\\n[\\W\\w]+")
+    @Pattern(regexp = "%[a-zA-Z]+\\n[\\W\\w]+")
     private String code;
 
-    //@Pattern(regexp = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")
+    @Pattern(regexp = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")
     private String sessionId;
 
     public String getCode() {
